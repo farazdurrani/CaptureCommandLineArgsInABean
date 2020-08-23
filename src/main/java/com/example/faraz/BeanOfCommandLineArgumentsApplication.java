a5b33c7cbb4e55330cad751de2c67c2441fedbff
Faraz Durrani <faraz.uic@gmail.com>
2020-08-23 14:29:45 -0500

    some changes

--- src/main/java/com/example/faraz/BeanOfCommandLineArgumentsApplication.java
+++ src/main/java/com/example/faraz/BeanOfCommandLineArgumentsApplication.java
@@ -3,12 +3,16 @@ package com.example.faraz;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BeanOfCommandLineArgumentsApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
@@ -21,6 +25,14 @@ public class BeanOfCommandLineArgumentsApplication implements CommandLineRunner 
     @Override
     public void run(String... args) throws Exception {
 	Arrays.stream(aa.getSourceArgs()).forEach(System.err::println);
+    }
+    
+    @Value("${server.port:8080}")    
+    private String port;
+    
+    @GetMapping("/test")
+    public String test1() {
+	return "Haan bhaee from " + port;
     }
 
 }
